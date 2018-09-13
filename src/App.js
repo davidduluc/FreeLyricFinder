4ed618b3cb2d0d19250fcd98dd6c77a0d4c1e46a
Francisco Duluc <franduluc@gmail.com>
2018-09-13 00:03:08 -0500

    added lyrics page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-import moduleName from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import NavBar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+
+import { Provider } from "./context";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-      </div>
+      <Provider>
+        {" "}
+        <Router>
+          <React.Fragment>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
